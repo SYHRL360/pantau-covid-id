86e3130b2df7003a4815893da63d1a042277e023
SYAHRUL360 <not.syahrul@outlook.co.id>
2021-06-25 20:32:50 +0700

    add SpringBootServletInitializer to main class

--- src/main/java/com/The360company/portfolio/coronainfo/SpringBootCoronaInfoIdApplication.java
+++ src/main/java/com/The360company/portfolio/coronainfo/SpringBootCoronaInfoIdApplication.java
@@ -2,15 +2,26 @@ package com.The360company.portfolio.coronainfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class SpringBootCoronaInfoIdApplication {
+public class SpringBootCoronaInfoIdApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCoronaInfoIdApplication.class, args);
 	}
+	
+	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootCoronaInfoIdApplication.class);
+	}
+
+
 
 	// define bean for RestTemplate ... this is used to make client REST calls
 	@Bean
